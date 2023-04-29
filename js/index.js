bef5407f24e2c2ac8173563d702c3446f144d096
RobottiFranco <franco.robotti0@gmail.com>
2023-04-29 01:45:56 -0300

    add fetch color, ++generic addCard, modify HTML titules

--- js/index.js
+++ js/index.js
@@ -1,86 +1,107 @@
-/* varaibles */
+/* variables */
 
-const url = "https://my-json-server.typicode.com/RobottiFranco/API-FAKE/blob/main/users";
-
-//utilizado en la
-const button_add_card = document.querySelectorAll(".add-card")
-
-//utilizado en changeTitle
+const button_add_card = document.querySelectorAll(".add-card");
 const cardTitles = document.querySelectorAll('.title-card');
+const column = document.querySelectorAll(".column");
 
-//llamados
+/* llamados */
 changeTitle(cardTitles);
 
-
-
-
-/* este proceso se realiza para el poder acceder a los diferentes elementos que tengan la clase add-card
-esta seccion devuelve un numero i utilizado en addCard */
-
-for (let i = 0; i< button_add_card.length;i++){
-    button_add_card[i].addEventListener("click", function() {addCard(i)})
+/* este proceso se realiza para poder acceder a los diferentes elementos que tengan la clase add-card.
+esta sección devuelve un número "i" que se utiliza en addCard */
+for (let i = 0; i < button_add_card.length; i++) {
+  button_add_card[i].addEventListener("click", function() {
+    addCard(i, "user");
+  });
 }
 
-/* esta funcion tiene todos los parametros opcionales, esto se denota por la estructura de los parametros.
-en esta funcion quiero crear un estilo de "tarjetas" dentro de un div particular llamado colum,
-como la clase add-card esta contenida dentro del div con clase column, voy a utilizar el valor i obtenida en la funcion de arriba.
+/* desactualizado */
+/* esta función tiene todos los parámetros opcionales, esto se denota por la estructura de los parámetros.
+En esta función quiero crear un estilo de "tarjetas" dentro de un div particular llamado "column".
+Como la clase "add-card" está contenida dentro del div con clase "column", voy a utilizar el valor "i" obtenido en la función de arriba.
 
-datos a considerar: 
-- como  quiero crear varias estructuras dentro de la tarjeta, ademas de crearla, creo varias etiquetas y le asigno por dentro los valores
-de los parametros.
-- esta funcion tiene momentamente el llamado a deleted para borrar la tarjeta creada. 
+Datos a considerar:
+- Como quiero crear varias estructuras dentro de la tarjeta, además de crearla, creo varias etiquetas y les asigno los valores de los parámetros.
+- Esta función tiene momentáneamente el llamado a "deleted" para borrar la tarjeta creada. 
 */
 
-function addCard(i = 0, space = "Worker", name = "Julian", lastName = "Martinez", description = "good slave"){
+/* actual */
+/* la funcion toma muchos parametros pero no son obligatorios, lo que hace es segun el primer parametro en que fila va a insertar y el segundo que switch va a usar,
+esto para hacer la funcion mas generica y en un futuro agregar mas sin apenas tocar codigo, el resto de parametros son elementos dentro de cada card.
+Datos a considerar:
+- Como quiero crear varias estructuras dentro de la tarjeta, además de crearla, creo varias etiquetas y les asigno los valores de los parámetros.
+- Esta función tiene momentáneamente el llamado a "deleted" para borrar la tarjeta creada. 
+ */
 
-    const column = document.querySelectorAll(".column");
-    const card = document.createElement("div");
 
-    card.classList.add("card");
-    card.innerHTML = `
-    <h2>${space}</h2>
-    <p>Name: ${name} ${lastName}</p>
-    <p>Description: ${description}<p>`;
+function addCard(i, type, title, description, description2, description3) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  switch (type){
+    case "color":
+      addCardColor(card, title, description);
+      break;
+    case "user":
+      addCardUser(card, title, description, description2, description3);
+      break;
+  }
 
   column[i].appendChild(card);
-
   card.addEventListener("click", deleted);
 }
 
-/* esta es una funcion que borra la tarjeta entera, esta es momentanea mientras encuentro una forma mejor de realizar la tarea.
-lo que ocurre aqui es que como dentro de la funcion addCard tiene un card.listener de delete, todas las tarjetas creadas tienen
-un target que permite eliminarlas facilmente  */
+function addCardUser(card, role = "Worker", name = "Julian", lastName = "Martinez", description = "good slave") {
+  card.innerHTML = `
+    <h2>${role}</h2>
+    <p>Name: ${name} ${lastName}</p>
+    <p>Description: ${description}<p>`;
+}
+
+function addCardColor(card, role = "colors", color = "skyblue") {
+  card.innerHTML = `
+    <h2>${role}</h2>
+    <p>Colors: ${color}</p>`;
+}
+
+/* Esta es una función que borra la tarjeta entera. Esta es momentánea mientras encuentro una forma mejor de realizar la tarea.
+Lo que ocurre aquí es que, como dentro de la función "addCard" tiene un "card.listener" de "deleted", todas las tarjetas creadas tienen un target que permite eliminarlas fácilmente. */
 
 function deleted(event) {
   event.currentTarget.remove();
 }
 
-
-/* esta funcion cambia el titulo de forma similar a la primera, recorre todos los que tenga la clase title-card y toma el valor que necesita
-luego con ocurre un prompt que introduce el valor y lo coloca en el html*/
+/* Esta función cambia el título de forma similar a la primera. Recorre todos los elementos que tengan la clase "title-card" y toma el valor que necesita.
+Luego, ocurre un prompt que introduce el valor y lo coloca en el HTML. */
 
 function changeTitle(cardTitles) {
   for (let i = 0; i < cardTitles.length; i++) {
     (function(index) {
       cardTitles[index].addEventListener('click', function() {
-        const newTitle = prompt("Enter a new title:", cardTitles[index].innerHTML);
+        let newTitle = prompt("Enter a new title:", cardTitles[index].innerHTML);
         cardTitles[index].innerHTML = newTitle;
       });
     })(i);
   }
 }
 
+/* Este fetch es la forma en que hay en JS de tomar los datos de un JSON y colocarlos en el HTML. */
+fetch("https://my-json-server.typicode.com/RobottiFranco/API-FAKE/users")
+  .then(response => response.json())
+  .then(data => {
+    for (let u = 0; u < data.length; u++) {
+      addCard(0, "user", data[u].role, data[u].name, data[u].lastName, data[u].description);
+    }
+  })
+  .catch(err => console.log(err));
 
-/* Este fetch es la forma en que hay en js de tomar los datos de un json y colocarlos en el html
-lo que hago aqui es tomo el la url y transformo la respuesta en un json, en este caso particular
-tomo cada uno de los valores de cada usuario y los imprimo a partir de la funcion addCard
-el .catch es por posibles errores, es un punto a mejorar */
-
-fetch(url)
-.then(response => response.json() )
-.then(data => {
-  for(let u=0; u<=data.length; u++){
-    addCard(undefined, data[u].role, data[u].name, data[u].lastName, data[u].description);
-  }
-})
-.catch(err => console.log(err));
+/* son 2 diferentes con distintos datos y por lo tanto diferente implementacion */
+fetch("https://my-json-server.typicode.com/RobottiFranco/API-FAKE/colors")
+  .then(response => response.json())
+  .then(data => {
+    for (let e = 0; e < data.length; e++) {
+      addCard(1, "color", data[e].role, data[e].color);
+    }
+  })
+  .catch(err => console.log(err));
+ 
