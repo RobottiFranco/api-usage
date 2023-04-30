274f01217103eff66f7f709a6a7990b4db73e448
RobottiFranco <franco.robotti0@gmail.com>
2023-04-30 02:52:55 -0300

    change 'addCard' and the fetchs

--- js/index.js
+++ js/index.js
@@ -1,84 +1,90 @@
 /* variables */
 
-const button_add_card = document.querySelectorAll(".add-card");
-const cardTitles = document.querySelectorAll('.title-card');
-const column = document.querySelectorAll(".column");
+const button_add_card = document.querySelectorAll(".add-card"); //todos los elementos del body de clase add-card
+const cardTitles = document.querySelectorAll(".title-card"); //todos los elementos del body con la clase title-card
+const column = document.querySelectorAll(".column"); //todos los elelementos del body con la clase column
 
 /* llamados */
-changeTitle(cardTitles);
+changeTitle(cardTitles); //borra las tarjetas existentes y futuras creadas
 
-/* este proceso se realiza para poder acceder a los diferentes elementos que tengan la clase add-card.
-esta sección devuelve un número "i" que se utiliza en addCard */
+/* eventos */
+
+/* resumen : evento que elijo y agrego una tarjeta por defecto segun la posicion del boton */
 for (let i = 0; i < button_add_card.length; i++) {
-  button_add_card[i].addEventListener("click", function() {
-    addCard(i, "user");
+  //itero sobre todos los elementos hasta encontrar el llamado que me interesa antender
+  button_add_card[i].addEventListener("click", function () {
+    //le agrego el evento de agregar tarjeta al boton que busque
+    const addDefaultCard = {
+      //estos parametros son para usar en el addCard, ya que todos son opcionales. (es un objeto)
+      columnAdd: i,
+      type: "user",
+    };
+    addCard(addDefaultCard); //lamado a la fauncion
   });
 }
 
-/* desactualizado */
-/* esta función tiene todos los parámetros opcionales, esto se denota por la estructura de los parámetros.
-En esta función quiero crear un estilo de "tarjetas" dentro de un div particular llamado "column".
-Como la clase "add-card" está contenida dentro del div con clase "column", voy a utilizar el valor "i" obtenido en la función de arriba.
+/* resumen: la funcion toma parametros y segun ellos crea diferentes tipos de tarjeta y les da un target de delete */
+function addCard({
+  //el {} es para señalar que los parametros son opcionales y que reibe un objeto como parametro
+  columnAdd = 0,
+  type = "user",
+  title = "Worker",
+  name = "Julian",
+  lastName = "Martinez",
+  description = "good slave",
+}) {
+  const card = document.createElement("div"); //creacion del elemento card
+  card.classList.add("card"); //le doy una clase al elemento card recien creado
 
-Datos a considerar:
-- Como quiero crear varias estructuras dentro de la tarjeta, además de crearla, creo varias etiquetas y les asigno los valores de los parámetros.
-- Esta función tiene momentáneamente el llamado a "deleted" para borrar la tarjeta creada. 
-*/
-
-/* actual */
-/* la funcion toma muchos parametros pero no son obligatorios, lo que hace es segun el primer parametro en que fila va a insertar y el segundo que switch va a usar,
-esto para hacer la funcion mas generica y en un futuro agregar mas sin apenas tocar codigo, el resto de parametros son elementos dentro de cada card.
-Datos a considerar:
-- Como quiero crear varias estructuras dentro de la tarjeta, además de crearla, creo varias etiquetas y les asigno los valores de los parámetros.
-- Esta función tiene momentáneamente el llamado a "deleted" para borrar la tarjeta creada. 
- */
-
-
-function addCard(i, type, title, description, description2, description3) {
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  switch (type){
+  switch (
+    type //posibilidad de modificacion, se usa para seleccionar que clase llamara para continuar con la contruccion, dependiendo del type
+  ) {
     case "color":
       addCardColor(card, title, description);
       break;
     case "user":
-      addCardUser(card, title, description, description2, description3);
+      addCardUser(card, title, name, lastName, description);
       break;
+
+    default:
+      addCardUser(card, title, name, lastName, description);
   }
 
-  column[i].appendChild(card);
-  card.addEventListener("click", deleted);
+  column[columnAdd].appendChild(card); //agrego la card como hijo de la columna seleccionada.
+  card.addEventListener("click", deleted); //a la tarjeta creada le asigno un tarjet para poder borrarla
 }
 
-function addCardUser(card, role = "Worker", name = "Julian", lastName = "Martinez", description = "good slave") {
+/* resumen: da el formato al tipo de tarjeta de tipo user (primera columna) */
+function addCardUser(card, title, name, lastName, descriptionUser) {
   card.innerHTML = `
-    <h2>${role}</h2>
+    <h2>${title}</h2>
     <p>Name: ${name} ${lastName}</p>
-    <p>Description: ${description}<p>`;
+    <p>Description: ${descriptionUser}<p>`; //crea los elementos de la tarjeta
 }
 
-function addCardColor(card, role = "colors", color = "skyblue") {
-  card.innerHTML = `
+/* resumen: da el formato al tipo de tarjeta de tipo color (segunda columna) */
+function addCardColor(card, role, descriptionColor) {
+  card.innerHTML = ` 
     <h2>${role}</h2>
-    <p>Colors: ${color}</p>`;
+    <p>Colors: ${descriptionColor}</p>`; //crea los elementos de la tarjeta
 }
 
-/* Esta es una función que borra la tarjeta entera. Esta es momentánea mientras encuentro una forma mejor de realizar la tarea.
-Lo que ocurre aquí es que, como dentro de la función "addCard" tiene un "card.listener" de "deleted", todas las tarjetas creadas tienen un target que permite eliminarlas fácilmente. */
-
+/* resumen: borra las tarjetas creadas y por crear segun se elija mediante 1 click */
 function deleted(event) {
   event.currentTarget.remove();
 }
 
-/* Esta función cambia el título de forma similar a la primera. Recorre todos los elementos que tengan la clase "title-card" y toma el valor que necesita.
+/* Esta función cambia el título de l columna. Recorre todos los elementos que tengan la clase "title-card" y toma el valor que necesita.
 Luego, ocurre un prompt que introduce el valor y lo coloca en el HTML. */
 
 function changeTitle(cardTitles) {
   for (let i = 0; i < cardTitles.length; i++) {
-    (function(index) {
-      cardTitles[index].addEventListener('click', function() {
-        let newTitle = prompt("Enter a new title:", cardTitles[index].innerHTML);
+    (function (index) {
+      cardTitles[index].addEventListener("click", function () {
+        let newTitle = prompt(
+          "Enter a new title:",
+          cardTitles[index].innerHTML
+        );
         cardTitles[index].innerHTML = newTitle;
       });
     })(i);
@@ -87,21 +93,34 @@ function changeTitle(cardTitles) {
 
 /* Este fetch es la forma en que hay en JS de tomar los datos de un JSON y colocarlos en el HTML. */
 fetch("https://my-json-server.typicode.com/RobottiFranco/API-FAKE/users")
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     for (let u = 0; u < data.length; u++) {
-      addCard(0, "user", data[u].role, data[u].name, data[u].lastName, data[u].description);
+      const addUserCardParams = {
+        columnAdd: 0,
+        type: "user",
+        title: data[u].role,
+        name: data[u].name,
+        lastName: data[u].lastName,
+        description: data[u].description,
+      };
+      addCard(addUserCardParams);
     }
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 /* son 2 diferentes con distintos datos y por lo tanto diferente implementacion */
 fetch("https://my-json-server.typicode.com/RobottiFranco/API-FAKE/colors")
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     for (let e = 0; e < data.length; e++) {
-      addCard(1, "color", data[e].role, data[e].color);
+      const addColorCardParams = {
+        columnAdd: 1,
+        type: "color",
+        title: data[e].role,
+        description: data[e].color,
+      };
+      addCard(addColorCardParams);
     }
   })
-  .catch(err => console.log(err));
- 
+  .catch((err) => console.log(err));
